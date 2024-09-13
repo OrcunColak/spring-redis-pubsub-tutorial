6cd8281e47aa9700db69d706bb75524e40f20234
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-13 14:27:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Read Me
+
+The original idea is from
+
+https://towardsdev.com/cqrs-in-action-building-robust-micro-services-with-spring-boot-68faa9133a4d
+
+
---
+++ src/main/java/com/colak/springtutorial/consumer/RedisEventListener.java
@@ -0,0 +1,13 @@
+package com.colak.springtutorial.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RedisEventListener {
+
+    public void listen(Object message) {
+        log.info("New event received: {}", message);
+    }
+}
